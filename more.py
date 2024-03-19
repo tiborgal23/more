6fb7672e51061f9b25e58b7c1cc74477331d8a12
tiborgal23 <independentyew@gmail.com>
2024-03-19 13:35:38 +0100

    pole

---
+++ more.py
@@ -0,0 +1,11 @@
+cislo = int(1)
+cislo_float = float(3.23)
+text = str ("String je sada znaků, například pro text")
+Boolean = True
+
+arrays = ["Volkswagen", "Mercedes", "Porsche", "Audi", "Opel", "BMW"]
+
+print(arrays[0])
+
+for x in range(6):
+    print(x+1, arrays[x])#volame x(0) +1
